99255e9c7c4edf2efa04ddab4b656c52a7462508
Coalery <doralife12@naver.com>
2020-04-03 21:56:35 +0900

    doldom command add

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,19 @@ import javax.swing.JFrame;
 import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.NickAlreadyInUseException;
 
-public class goFile {
+public class Main {
 
-    private static final String OAUTH = Oauth.oauth;
     private static final String ADDRESS = "irc.twitch.tv.";
     private static final int PORT = 6667;
     private static final String NICKNAME = "MYNICKHERE";
-    private static final String CHANNEL = "#doralife12";
+    public static final String CHANNEL = "#doralife12";
 
     public static void main(String[] args) throws NickAlreadyInUseException, IOException, IrcException {
-        TwitchBot bot = new TwitchBot(NICKNAME);
+        TwitchBot bot = new TwitchBot(CHANNEL, NICKNAME);
         bot.setVerbose(false);
 
-        bot.connect(ADDRESS, PORT, OAUTH);
+        bot.connect(ADDRESS, PORT, Oauth.OATUH);
         bot.joinChannel(CHANNEL);
-        
-        bot.sendMessage(CHANNEL, "러리봇이 켜졌습니다.");
         
         JFrame frame = new JFrame();
         frame.setSize(400, 300);
--- src/TwitchBot.java
+++ src/TwitchBot.java
@@ -2,7 +2,10 @@ import org.jibble.pircbot.PircBot;
 
 public class TwitchBot extends PircBot {
 	
-	public TwitchBot(String nick) {
+	private String channel;
+	
+	public TwitchBot(String channel, String nick) {
+		this.channel = channel;
 		setName(nick);
 		setLogin(nick);
 	}
@@ -14,6 +17,38 @@ public class TwitchBot extends PircBot {
         
         if (line.startsWith(":")) {
             String[] recvLines = line.split(" ");
+            if(recvLines[1].equalsIgnoreCase("PRIVMSG")) {
+            	String sender = recvLines[0].split("!")[0].substring(1);
+            	StringBuilder message = new StringBuilder(recvLines[3].substring(1));
+            	for(int i=4; i<recvLines.length; i++)
+            		message.append(" " + recvLines[i]);
+            	String result = recieveMessage(sender, message.toString());
+            	System.out.println("[Result] " + result);
+            	if(result != null)
+            		sendMessage(channel, "@" + sender + " : " + result);
+            }
         }
+    }
+    
+    private String recieveMessage(String sender, String message) {
+    	String[] split = message.split(" ");
+    	System.out.println("[" + message + "]");
+    	if(split[0].equals("!러리봇"))
+    		return "띠링";
+    	if(split[0].equals("!물고기")) {
+    		if(split.length == 1)
+    			return "!물고기 <정보/목록>";
+    	}
+    	if(split[0].equals("!돌돔")) {
+    		if(split.length == 1)
+    			return "!돌돔 <벨>";
+    		try {
+    			int val = Integer.parseInt(split[1]);
+    			return val / 5000.0f + "돌돔";
+    		} catch(NumberFormatException e) {
+    			return "숫자 값을 입력해주세요.";
+    		}
+    	}
+    	return null;
     }
 }
